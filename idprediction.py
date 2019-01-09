6e7da75e135ff5e2b195997080a9baf498ef5641
CrazyFox-001 <46416788+CrazyFox-001@users.noreply.github.com>
2019-01-09 22:52:50 +0530

    Update idprediction.py

--- idprediction.py
+++ idprediction.py
@@ -1,13 +1,14 @@
 import numpy as np
-from keras.models import Sequential
 from keras.models import  load_model
 from keras.preprocessing import image
 
-testimage = image.load_img('nic/prediction/2.jpg',target_size=(64,64))
+testimage = image.load_img('predict/13.jpg',target_size=(64,64))
 
 testimage = image.img_to_array(testimage)
 testimage = np.expand_dims(testimage,0)
-model = load_model('id.h5')
+model = load_model('id2.h5')
 results =model.predict(testimage)
+
+
 
 print (results)
